00a38cf955ad6e9f88b339e3f5c7b115b8f9704c
Benjamin <benjamin.button.code@gmail.com>
2021-04-13 21:11:09 +0300

    Refresh

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
-import classes
+import sys
+import random
+
+import pygame
 
 
-game = classes.Game()
-snake = classes.Snake(game.GREEN)
-food = classes.Food(game.RED, game.WIDTH, game.HEIGHT)
+pygame.init()
 
-classes.Game.init_and_check_for_errors()
+# Settings / Constatns
+CELL_SIZE = 40
+CELL_NUMBER = 18
+WIDTH = CELL_NUMBER * CELL_SIZE
+HEIGHT = WIDTH
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+CLOCK = pygame.time.Clock()
+FPS = 30
+GAME_LOOP = True
 
-while True:
-    # Get direction
-    snake.change_to = game.event_loop(snake.change_to)
-
-    # Movement
-    snake.validate_direction_and_change()
-    snake.change_head_position()
-    # Update (if it's need) score and food
-    game.score, food.food_pos = snake.snake_body_mechanism(game.score, food.food_pos,
-                                                           game.WIDTH, game.HEIGHT)
-
-    # Drawing
-    snake.draw_snake(game.display_surface, game.BLACK)
-    food.draw_food(game.display_surface)
-
-    # Check for collisions
-    snake.check_for_boundaries(game.game_over, game.WIDTH, game.HEIGHT)
-
-    # Show and update screen
-    game.show_score()
-    game.update_screen()
+while GAME_LOOP:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                pass
+            elif event.key == pygame.K_LEFT or event.key == ord('a'):
+                pass
+            elif event.key == pygame.K_UP or event.key == ord('w'):
+                pass
+            elif event.key == pygame.K_DOWN or event.key == ord('s'):
+                pass
+            elif event.key == pygame.K_ESCAPE:
+                pass
+    
+    pygame.display.update()
+    CLOCK.tick(FPS)
