f6a2dd68fea957d9166fe6f0d0456f0c7753268b
Benjamin <benjamin.button.code@gmail.com>
2021-04-14 21:03:31 +0300

    add graphic for snake

--- main.py
+++ main.py
@@ -25,6 +25,24 @@ LEFT = Vector2(-1, 0)
 DOWN = Vector2(0, 1)
 UP = Vector2(0, -1)
 
+# Load images of snake
+snake_head_down = pygame.image.load("Images/snake_head_down.png").convert_alpha()
+snake_head_up = pygame.image.load("Images/snake_head_up.png").convert_alpha()
+snake_head_right = pygame.image.load("Images/snake_head_right.png").convert_alpha()
+snake_head_left = pygame.image.load("Images/snake_head_left.png").convert_alpha()
+
+snake_tail_up = pygame.image.load("Images/snake_tail_up.png").convert_alpha()
+snake_tail_down = pygame.image.load("Images/snake_tail_down.png").convert_alpha()
+snake_tail_right = pygame.image.load("Images/snake_tail_right.png").convert_alpha()
+snake_tail_left = pygame.image.load("Images/snake_tail_left.png").convert_alpha()
+
+snake_body_vertical = pygame.image.load("Images/snake_body_vertical.png").convert_alpha()
+snake_body_horizontal = pygame.image.load("Images/snake_body_horizontal.png").convert_alpha()
+snake_body_br = pygame.image.load("Images/snake_body_br.png").convert_alpha()
+snake_body_bl = pygame.image.load("Images/snake_body_bl.png").convert_alpha()
+snake_body_lt = pygame.image.load("Images/snake_body_lt.png").convert_alpha()
+snake_body_rt = pygame.image.load("Images/snake_body_rt.png").convert_alpha()
+
 SPEEDUP = True
 GAME_FONT = pygame.font.Font("fonts/Neucha-Regular.ttf", 25)
 GAME_LOOP = True
@@ -34,17 +52,69 @@ class Snake:
         self.body = [Vector2(7, 9), Vector2(6, 9), Vector2(5, 9)]
         self.direction = RIGTH
 
+        # Images
+        self.head = snake_head_right
+        self.tail = snake_tail_right
+
     def movement(self, sneck=False):
         self.body.insert(0, self.body[0] + self.direction)
         if not sneck:
             self.body.pop()
         
+    def snake_head_update(self):
+        if self.direction == RIGTH:
+            self.head = snake_head_right
+        elif self.direction == LEFT:
+            self.head = snake_head_left
+        elif self.direction == UP:
+            self.head = snake_head_up
+        else:
+            self.head = snake_head_down
+    
+    def snake_tail_update(self):
+        relation_tail_body = self.body[-1] - self.body[-2]
+        if relation_tail_body == RIGTH:
+            self.tail = snake_tail_right
+        elif relation_tail_body == LEFT:
+            self.tail = snake_tail_left
+        elif relation_tail_body == UP:
+            self.tail = snake_tail_up
+        else:
+            self.tail = snake_tail_down
+
     def draw(self):
-        for block in self.body:
+        self.snake_head_update()
+        self.snake_tail_update()
+
+        for index, block in enumerate(self.body):
             x_pos = int(block.x * CELL_SIZE)
             y_pos = int(block.y * CELL_SIZE)
             block_rect = pygame.Rect(x_pos, y_pos, CELL_SIZE - 2, CELL_SIZE - 2)
-            pygame.draw.rect(SCREEN, (0, 255, 0), block_rect)
+
+            if index == 0:
+                SCREEN.blit(self.head, block_rect)
+            elif index == len(self.body) - 1:
+                SCREEN.blit(self.tail, block_rect)
+            else:
+                previos_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previos_block.x == next_block.x:
+                    SCREEN.blit(snake_body_vertical, block_rect)
+                elif previos_block.y == next_block.y:
+                    SCREEN.blit(snake_body_horizontal, block_rect)
+                else:
+                    if next_block.x == 1 and previos_block.y == 1 or \
+                        next_block.y == 1 and previos_block.x == 1:
+                        SCREEN.blit(snake_body_br, block_rect)
+                    elif next_block.x == -1 and previos_block.y == 1 or \
+                        next_block.y == 1 and previos_block.x == -1:
+                        SCREEN.blit(snake_body_bl, block_rect)
+                    elif next_block.x == 1 and previos_block.y == -1 or \
+                        next_block.y == -1 and previos_block.x == 1:
+                        SCREEN.blit(snake_body_rt, block_rect)
+                    else:
+                        SCREEN.blit(snake_body_lt, block_rect)
+
 
 
 class Food:
